f02bd84727ab46a8e787c135fe3060b8db4cb554
Muayyad1999 <37573322+Muayyad1999@users.noreply.github.com>
2021-09-30 22:39:21 +0300

    Update python-pass.py

--- python-pass.py
+++ python-pass.py
@@ -1,7 +1,15 @@
-
-
 class Solution:
+    def longestPalindrome(s: str) -> str:
+        temp = ''  # Temporary Storing
+        for i in range(len(s)):
+            for j in range(len(s), i, -1):
+                if len(temp) >= j-i:
+                    break
+                elif s[i:j] == s[i:j][::-1]:
+                    temp = s[i:j]
+                    break
+        return temp
 
-    @staticmethod
-    def longest_palindromic(s: str) -> str:
-        pass
+Words=['babad','cbbd','ac','a',]
+for k in Words:
+    print(Solution.longestPalindrome(k))
